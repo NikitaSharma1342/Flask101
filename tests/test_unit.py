bc2af7699929b5c4774259ce6142373f5373b6f7
Nikita Sharma <103064401+NikitaSharma1342@users.noreply.github.com>
2024-09-14 20:43:05 +0530

    refactoring to divide src and tests folder

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,10 +1,10 @@
 import unittest
 from unittest.mock import patch
-from app import home
+from src.app import home
 
 class TestHomeFunction(unittest.TestCase):
 
-    @patch('app.render_template')
+    @patch('src.app.render_template')
     def test_home_content(self,mock_render_template):
         mock_render_template.return_value = "Mocked Template Response"
 
