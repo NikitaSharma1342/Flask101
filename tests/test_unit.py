bbc1373d3a999191f39f65c5ca7a000d4050c241
Nikita Sharma <103064401+NikitaSharma1342@users.noreply.github.com>
2024-09-15 17:51:15 +0530

    update unit test for POST route in registration

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,17 +1,32 @@
 import unittest
 from unittest.mock import patch
-from src.app import home
+from src.app import home, app
+
 
 class TestHomeFunction(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
 
     @patch('src.app.render_template')
-    def test_home_content(self,mock_render_template):
+    def test_home_content(self, mock_render_template):
         mock_render_template.return_value = "Mocked Template Response"
 
         # Test that the home function returns the correct content
         response = home()
         mock_render_template.assert_called_with('home.html')
         self.assertEqual(response, "Mocked Template Response")
+
+    @patch('src.app.show_registration_form')
+    def test_post_registration_data(self, mock_show_registration_form):
+        # Set the mock return value to simulate successful form submission
+        mock_show_registration_form.return_value = 'Welcome, Hello World!'
+
+        # Simulate the POST request with form data
+        with app.test_request_context('/register', method='POST', data={'first_name': 'Hello', 'last_name': 'World'}):
+            response = app.view_functions['show_registration_form']()
+            self.assertIn('Welcome, Hello World!', response)
+
 
 if __name__ == '__main__':
     unittest.main()
